e4ed549c6b64535f77204a116578b61bd181beb0
collins <collinsboniface1000@gmail.com>
2022-01-02 10:18:48 +0200

    Created a text field for the color and adding a constructor for the class

--- lib/screens/location_detail/text_section.dart
+++ lib/screens/location_detail/text_section.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TextSection extends StatelessWidget {
+
+  Color _color;
+
+  TextSection(this._color);
+
   @override
   Widget build(BuildContext context) {
     return (
      Container(
        child: const Text("Hello World"),
 
-       decoration: const BoxDecoration(
-         color: Colors.red,
+       decoration: BoxDecoration(
+         color: _color,
        ),
 
      )
