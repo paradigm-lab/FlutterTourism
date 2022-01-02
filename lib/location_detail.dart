61865d7ec197a1d91661e7f3b38da2293b9af402
collins <collinsboniface1000@gmail.com>
2022-01-02 09:18:36 +0200

    commenting out the import of the package

---
+++ lib/location_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello"),
+      ),
+      body: const Text("Structure of the application"),
+    );
+  }
+
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_practice/location_detail/location_detail.dart';
+//import 'package:flutter_practice/location_detail/location_detail.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
