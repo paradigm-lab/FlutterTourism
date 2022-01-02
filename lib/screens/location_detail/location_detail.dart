29653ee1181c7835b08313daa67c671214e43b6c
collins <collinsboniface1000@gmail.com>
2022-01-02 21:02:07 +0200

    Changing the Text in the instance of the TextSection

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 import './image_banner.dart';
 import './text_section.dart';
 
+
 class LocationDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        appBar: AppBar(
+          title: const Text("Tourism"),
+          backgroundColor: Colors.red,
+        ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
             ImageBanner("assets/images/zanzibar.jpeg"),
-            TextSection("Summary", "The boundless wilderness and big game of Tanzania has long attracted adventure seekers from all over the world."),
-            TextSection("Summary", "No other african country presents such an incredible range of landscapes, unique geographical features and species like Tanzania."),
-            TextSection("Summary", "Unique geographical features and species like Tanzania."),
+            TextSection("Arusha", "Arusha City is a Tanzanian city and the regional capital of the Arusha Region, with a population of 416,442 plus 323,198 in the surrounding Arusha Rural District (2012 census)."),
+            TextSection("Dar-es-salaam", "Dar es Salaam is Tanzania's most prominent city for arts, fashion, media, film, television, and finance. It is the capital of the co-extensive Dar es Salaam Region, one of Tanzania's 31 administrative regions"),
+            TextSection("Zanzibar", "Zanzibar's marine ecosystem is an important part of the economy for fishing and algaculture and contains important marine ecosystems that act as fish nurseries for Indian Ocean fish populations. Moreover,"),
         ],
       ),
     );
--- lib/screens/location_detail/text_section.dart
+++ lib/screens/location_detail/text_section.dart
@@ -19,7 +19,7 @@ class TextSection extends StatelessWidget {
           children: [
             Container(
                 padding: const EdgeInsets.fromLTRB(_hPad, 32.0, _hPad, 4.0),
-                child: Text(_title),
+                child: Text(_title, style: Theme.of(context).textTheme.subtitle1),
             ),
 
             Container(
