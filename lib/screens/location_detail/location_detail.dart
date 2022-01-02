19a4216e40057c3f62b33a5291defdbd7f19bc81
collins <collinsboniface1000@gmail.com>
2022-01-02 21:06:56 +0200

    Redused the number of text in the TextSection instance

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -17,9 +17,9 @@ class LocationDetail extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
             ImageBanner("assets/images/zanzibar.jpeg"),
-            TextSection("Arusha", "Arusha City is a Tanzanian city and the regional capital of the Arusha Region, with a population of 416,442 plus 323,198 in the surrounding Arusha Rural District (2012 census)."),
-            TextSection("Dar-es-salaam", "Dar es Salaam is Tanzania's most prominent city for arts, fashion, media, film, television, and finance. It is the capital of the co-extensive Dar es Salaam Region, one of Tanzania's 31 administrative regions"),
-            TextSection("Zanzibar", "Zanzibar's marine ecosystem is an important part of the economy for fishing and algaculture and contains important marine ecosystems that act as fish nurseries for Indian Ocean fish populations. Moreover,"),
+            TextSection("Arusha", "Arusha City is a Tanzanian city and the regional capital of the Arusha Region, with a population of 416,442 plus 323,198."),
+            TextSection("Dar-es-salaam", "Dar es Salaam is Tanzania's most prominent city for arts, fashion, media, film, television, and finance."),
+            TextSection("Zanzibar", "Zanzibar's marine ecosystem is an important part of the economy for fishing and algaculture and contains important marine."),
         ],
       ),
     );
