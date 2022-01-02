011de56d377b713b836531d4576158a44c87efc1
collins <collinsboniface1000@gmail.com>
2022-01-02 21:03:18 +0200

    Added the ThemeData in the App by adding the appBarTheme

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/screens/location_detail/location_detail.dart';
+import './style.dart';
 
 class App extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
       return MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            textTheme: TextTheme(
+                subtitle2: AppBarTextStyle
+            ),
+          ),
+          textTheme: const TextTheme(
+            subtitle1: TitleTextStyle,
+            bodyText1: Body1TextStyle,
+          )
+        ),
         home: LocationDetail(),
       );
   }
