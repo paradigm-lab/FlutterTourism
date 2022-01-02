3b45eff15b7aedb928ab055a2528a13fa3cca255
collins <collinsboniface1000@gmail.com>
2022-01-02 20:28:07 +0200

    Added more text to the constructor of the TextSection class

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -7,17 +7,15 @@ class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello"),
-      ),
+
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
             ImageBanner("assets/images/zanzibar.jpeg"),
-            TextSection("Summary", "Something1"),
-            TextSection("Summary", "Something2"),
-            TextSection("Summary", "Something3"),
+            TextSection("Summary", "The boundless wilderness and big game of Tanzania has long attracted adventure seekers from all over the world."),
+            TextSection("Summary", "No other african country presents such an incredible range of landscapes, unique geographical features and species like Tanzania."),
+            TextSection("Summary", "Unique geographical features and species like Tanzania."),
         ],
       ),
     );
