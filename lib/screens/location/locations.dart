4f2e590c22944f433b630f2f715f040236caae4f
collins <collinsboniface1000@gmail.com>
2022-01-03 21:46:17 +0200

    Added the return type of the widget whereby we return the scaffold and AppBar with the  body whereby the body takes the ListView

--- lib/screens/location/locations.dart
+++ lib/screens/location/locations.dart
@@ -6,6 +6,16 @@ import '../../models/location.dart';
 class Locations extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      
+      final locations = Location.fetchAll();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Locations'),
+        ),
+        body: ListView(
+          children:
+            locations.map((location) => Text(location.name)).toList(),
+        ),
+      );
   }
 }
