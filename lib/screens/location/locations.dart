2e055d73b606d505c06bf3f19148e8cde7296aa3
collins <collinsboniface1000@gmail.com>
2022-01-03 21:35:39 +0200

    Created the class and added the import for the app and location whereby the locations extends Stateless

---
+++ lib/screens/location/locations.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../app.dart';
+import '../../models/location.dart';
+
+
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      
+  }
+}
