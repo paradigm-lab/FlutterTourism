f67714e89f2816bad406fe1fefe966b98a5afade
collins <collinsboniface1000@gmail.com>
2022-01-03 13:11:13 +0200

    Created a final location variable that that return the first location and change the AppBar name by using the model and we also invoce the imagePath into the imageBanner

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -8,16 +8,22 @@ class LocationDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Tourism"),
+          title: Text(location.name),
           backgroundColor: Colors.red,
         ),
+
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-            ImageBanner("assets/images/zanzibar.jpeg"),
+            ImageBanner(location.imagePath),
             TextSection("Arusha", "Arusha City is a Tanzanian city and the regional capital of the Arusha Region, with a population of 416,442 plus 323,198."),
             TextSection("Dar-es-salaam", "Dar es Salaam is Tanzania's most prominent city for arts, fashion, media, film, television, and finance."),
             TextSection("Zanzibar", "Zanzibar's marine ecosystem is an important part of the economy for fishing and algaculture and contains important marine."),
