ae73cd5c23a8de4d22d0c78601b74706c02cbbbf
collins <collinsboniface1000@gmail.com>
2022-01-09 22:29:58 +0200

    Removed the route in the build method and use it in the specific class

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -3,28 +3,52 @@ import 'package:flutter_practice/screens/location/locations.dart';
 import 'package:flutter_practice/screens/location_detail/location_detail.dart';
 import './style.dart';
 
+
+const LocationsRoute = '/';
+const LocationDetailRoute = '/location_detail';
+
+
 class App extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
       return MaterialApp(
         home: Locations(),
-        routes: {
-          '/': (context) => FirstScreen(),
-          '/second': (context) => SecondScreen(),
-        },
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            textTheme: TextTheme(
-                subtitle2: AppBarTextStyle
-            ),
-          ),
-          textTheme: const TextTheme(
-            subtitle1: TitleTextStyle,
-            bodyText1: Body1TextStyle,
-          )
-        ),
+        onGenerateRoute: _routes(),
+        theme: _theme(),
       );
+  }
+
+  RouteFactory _routes() {
+    return (settings) {
+      final Object? arguments = settings.arguments;
+      Widget screen;
+      switch (settings.name) {
+        case LocationsRoute:
+          screen = Locations();
+          break;
+        case LocationDetailRoute:
+          screen = LocationDetail(arguments["id"]);
+          break;
+        default:
+          return null;
+      }
+      return MaterialPageRoute(builder: (BuildContext context) => screen);
+    };
+  }
+
+  ThemeData _theme() {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+    textTheme: TextTheme(
+        subtitle2: AppBarTextStyle
+      ),
+    ),
+    textTheme: const TextTheme(
+        subtitle1: TitleTextStyle,
+        bodyText1: Body1TextStyle,
+      )
+    );
   }
 
 }
